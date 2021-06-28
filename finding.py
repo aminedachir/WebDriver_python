621496ae65bc420f50033df5f6d535c7fd6ef1fe
aminedachir <dachiramine110@gmail.com>
2021-06-28 14:42:48 +0100

    By

--- finding.py
+++ finding.py
@@ -1,2 +1,7 @@
  from selenium import webdriver
- 
+
+ from selenium.webdriver.common.by import By
+
+ driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
+
+ driver.get('http://fb.com')
