1ecfe89c16d6427478a033acb6e50ba54991d75a
aminedachir <dachiramine110@gmail.com>
2021-06-28 15:12:37 +0100

    find element

--- practice.py
+++ practice.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
 driver.get('http://127.0.0.1:5000/')
 
+input_field = driver.find_element(By.ID, 'email')
+print(input_field)
