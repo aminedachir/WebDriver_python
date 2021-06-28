84bef9b9d09e5f21e09cc8274be890977609ca7f
aminedachir <dachiramine110@gmail.com>
2021-06-28 15:39:10 +0100

    pdb

--- practice.py
+++ practice.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import pdb
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
@@ -8,6 +9,8 @@ driver.get('http://127.0.0.1:5000/')
 
 email_field = driver.find_element(By.ID, 'email')
 email_field.send_keys("dachiramine0@gmail.com")
+
+pdb.set_trace()
 
 password_field = driver.find_element(By.ID, 'password')
 password_field.send_keys("amineamine")
