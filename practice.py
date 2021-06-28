fbd56dc4ba1903e0df8471814d7225d03aedc46a
aminedachir <dachiramine110@gmail.com>
2021-06-28 15:46:29 +0100

    quit

--- practice.py
+++ practice.py
@@ -8,9 +8,7 @@ driver.get('http://127.0.0.1:5000/')
 
 email_field = driver.find_element(By.ID, 'email')
 email_field.send_keys("dachiramine0@gmail.com")
-
 driver.quit()
-
 password_field = driver.find_element(By.ID, 'password')
 password_field.send_keys("amineamine")
 password_field.send_keys(Keys.ENTER)
