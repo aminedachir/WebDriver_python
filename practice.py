a913ea039d71b43eb5b627557c2e67c12e1ef6f6
aminedachir <dachiramine110@gmail.com>
2021-06-28 15:42:59 +0100

    remove pdb

--- practice.py
+++ practice.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import pdb
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
@@ -9,8 +8,6 @@ driver.get('http://127.0.0.1:5000/')
 
 email_field = driver.find_element(By.ID, 'email')
 email_field.send_keys("dachiramine0@gmail.com")
-
-pdb.set_trace()
 
 password_field = driver.find_element(By.ID, 'password')
 password_field.send_keys("amineamine")
