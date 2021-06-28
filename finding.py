ded862b02b2234d01947adc279c9f04aac6528be
aminedachir <dachiramine110@gmail.com>
2021-06-28 14:56:41 +0100

    find element by id

--- finding.py
+++ finding.py
@@ -1,7 +1,11 @@
- from selenium import webdriver
+from selenium import webdriver
 
- from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 
- driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
+driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
- driver.get('http://fb.com')
+driver.get('http://amazon.com')
+
+covid = driver.find_element(By.ID, 'swm-link')
+print(covid)
+ 
