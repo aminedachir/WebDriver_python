26d350621ef699d327ab6e0b8d94efe4e1c6c4cc
aminedachir <dachiramine110@gmail.com>
2021-06-28 15:15:38 +0100

    email_field

--- practice.py
+++ practice.py
@@ -6,7 +6,8 @@ driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver'
 
 driver.get('http://127.0.0.1:5000/')
 
-input_field = driver.find_element(By.ID, 'email')
-input_field.send_keys("dachiramine0@gmail.com")
-input_field.send_keys(Keys.ENTER)
-print(input_field)
+email_field = driver.find_element(By.ID, 'email')
+email_field.send_keys("dachiramine0@gmail.com")
+email_field.send_keys(Keys.ENTER)
+
+
