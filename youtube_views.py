3ddb755d3fdbba9b296cbe4638f6b365bc2a5fc1
aminedachir <dachiramine110@gmail.com>
2021-06-30 00:10:27 +0100

    finish projeoject

--- youtube_views.py
+++ youtube_views.py
@@ -5,15 +5,16 @@ import time
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
-driver.get('https://www.youtube.com/watch?v=_CUaON7DQDI')
+driver.get('https://www.youtube.com/watch?v=mlZj910ey-I')
 
 watch_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-cued-thumbnail-overlay > button')
 watch_vedio.click()
-
-time_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate > span.ytp-time-duration')
-timevd = time_vedio.text
-print(timevd)
-
-time.sleep(timevd)
-driver.refresh()
-
+for i in range(100):
+    time_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate > span.ytp-time-duration')
+    timevd = time_vedio.text
+    if int(timevd[2:4] > 0):
+        r = int(timevd[0:2])
+    else:
+        r = int(timevd[2:4])
+    time.sleep(5)
+    driver.refresh()
