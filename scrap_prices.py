6970a8318353447aeec90c764b7236c9fa0e530a
aminedachir <dachiramine110@gmail.com>
2021-06-29 15:38:45 +0100

    print len of all product in website

--- scrap_prices.py
+++ scrap_prices.py
@@ -6,3 +6,4 @@ driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver'
 driver.get('http://demostore.supersqa.com')
 
 all_product = driver.find_elements(By.CLASS_NAME, 'product')
+print(len(all_product))
