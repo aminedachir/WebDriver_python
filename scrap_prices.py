c325bcc8c18ae6de083e0413b893d17b24f69d01
aminedachir <dachiramine110@gmail.com>
2021-06-29 15:36:57 +0100

    accses website

---
+++ scrap_prices.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
+
+driver.get('http://demostore.supersqa.com')
+
+#all_product
