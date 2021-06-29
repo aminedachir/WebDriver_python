75402939ac3cdd7226f033338b07249d4c27144e
aminedachir <dachiramine110@gmail.com>
2021-06-30 00:57:39 +0100

    print time vedio and timevedio 2

--- youtube_views.py
+++ youtube_views.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
-
 driver.get('https://www.youtube.com/watch?v=mlZj910ey-I')
 
 watch_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-cued-thumbnail-overlay > button')
@@ -19,7 +18,6 @@ for i in range(100):
     if timevd == timevd2:
         time.sleep(1)
         driver.refresh()
-
 
     #if int(timevd[2:4] > 0):
         #r = int(timevd[0:2])
