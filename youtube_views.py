d2f9ae38d1187b80d3c97b328bdd2f3bf3ae426e
aminedachir <dachiramine110@gmail.com>
2021-06-29 23:46:47 +0100

    import time

--- youtube_views.py
+++ youtube_views.py
@@ -1,7 +1,6 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
@@ -9,10 +8,12 @@ driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver'
 driver.get('https://www.youtube.com/watch?v=_CUaON7DQDI')
 
 watch_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-cued-thumbnail-overlay > button')
+watch_vedio.click()
+
 time_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate > span.ytp-time-duration')
 timevd = time_vedio.text
 print(timevd)
-watch_vedio.click()
+
 time.sleep(timevd)
 driver.refresh()
 
