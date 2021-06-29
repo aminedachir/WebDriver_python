670c985e68c635b2be63dfe70a3bcf9532ee6a45
aminedachir <dachiramine110@gmail.com>
2021-06-29 23:45:30 +0100

    sleep time of vedio

--- youtube_views.py
+++ youtube_views.py
@@ -7,8 +7,6 @@ import time
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
 driver.get('https://www.youtube.com/watch?v=_CUaON7DQDI')
-#time.sleep(1)
-#driver.refresh()
 
 watch_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-cued-thumbnail-overlay > button')
 time_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate > span.ytp-time-duration')
