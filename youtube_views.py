d1147be9bcc116dc0ba878ff934d2201bdf17396
aminedachir <dachiramine110@gmail.com>
2021-06-29 23:41:59 +0100

    play vedio

--- youtube_views.py
+++ youtube_views.py
@@ -1,6 +1,14 @@
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
 driver.get('https://www.youtube.com/watch?v=_CUaON7DQDI')
+#time.sleep(1)
+#driver.refresh()
+
+watch_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-cued-thumbnail-overlay > button')
+watch_vedio.click()
