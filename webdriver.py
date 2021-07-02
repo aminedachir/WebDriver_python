dc40e734bf544ca363585cb7ec249ddd51ff8886
aminedachir <dachiramine110@gmail.com>
2021-07-02 16:04:25 +0100

    youtube.com

--- webdriver.py
+++ webdriver.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
-driver.get('http://fb.com')
+driver.get('http://youtube.com')
 print(driver.title)
