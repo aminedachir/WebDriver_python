0ae852d173c94eb06e4e86e000a2f3a6c62997ad
aminedachir <dachiramine110@gmail.com>
2021-07-04 22:00:33 +0100

    print current url

--- offline_game_player.py
+++ offline_game_player.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
 driver.get('http://fb.com')
-
-dayn = driver.find_element(By.CSS_SELECTOR, "#main-frame-error > div.runner-container > canvas")
-#dayn.send_keys(Keys)
+time.sleep(3)
+#for i in range (10):
+    #dayn = driver.find_element(By.CLASS_NAME, "runner-canvas")
+print(current_url)
