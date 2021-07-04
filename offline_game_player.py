26c5a045b2a5b8ad9bee7c31aa450bf17c6a6155
aminedachir <dachiramine110@gmail.com>
2021-07-04 14:18:12 +0100

    import Keys

--- offline_game_player.py
+++ offline_game_player.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
 driver.get('http://fb.com')
 
 dayn = driver.find_element(By.CSS_SELECTOR, "#main-frame-error > div.runner-container > canvas")
+#dayn.send_keys(Keys)
