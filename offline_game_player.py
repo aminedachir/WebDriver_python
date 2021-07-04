a2fee8402523f2c270418b24c20e92aa77069206
aminedachir <dachiramine110@gmail.com>
2021-07-04 14:17:27 +0100

    find element

--- offline_game_player.py
+++ offline_game_player.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
 driver.get('http://fb.com')
+
+dayn = driver.find_element(By.CSS_SELECTOR, "#main-frame-error > div.runner-container > canvas")
