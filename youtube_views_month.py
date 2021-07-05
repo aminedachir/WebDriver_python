a7d58499c82161595bd8aebc82b6c22ac28a08c4
aminedachir <dachiramine110@gmail.com>
2021-07-05 13:48:09 +0100

    acces chaine

--- youtube_views_month.py
+++ youtube_views_month.py
@@ -1,1 +1,5 @@
 from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path =  '/home/amine/Downloads/Chromedriver')
+
+driver.get('https://www.youtube.com/channel/UCaj2GeIsW6Xll26js9o4seQ')
