6823dbac9a725e87c969c308e252837143f6c2b7
aminedachir <dachiramine110@gmail.com>
2021-07-05 14:02:17 +0100

    solve problem

--- youtube_views_month.py
+++ youtube_views_month.py
@@ -6,5 +6,5 @@ driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver'
 driver.get('https://www.youtube.com/channel/UCaj2GeIsW6Xll26js9o4seQ')
 videos = driver.find_element(By.CSS_SELECTOR, '#tabsContent > tp-yt-paper-tab:nth-child(4) > div')
 videos.click()
-all_videos = driver.find_element(By.CSS_SELECTOR, '#meta')
+all_videos = driver.find_elements(By.CSS_SELECTOR, '#meta')
 print(len(all_videos))
