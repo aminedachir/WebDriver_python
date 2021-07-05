455feef416e634c1d1a0f90d465191178e8d92b2
aminedachir <dachiramine110@gmail.com>
2021-07-05 23:13:39 +0100

    finish project succefully

--- youtube_views.py
+++ youtube_views.py
@@ -4,9 +4,9 @@ from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
-driver.get('https://www.youtube.com/watch?v=mlZj910ey-I')
+driver.get('https://www.youtube.com/watch?v=2-Zfr84-H7s')
 watch_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-cued-thumbnail-overlay > button')
 watch_vedio.click()
 for i in range(100):
-    time.sleep(60)
+    time.sleep(2)
     driver.refresh()
