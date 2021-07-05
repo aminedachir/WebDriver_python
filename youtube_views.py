04a41d25ec583e498bda1ae84978aab6caccccb3
aminedachir <dachiramine110@gmail.com>
2021-07-05 16:16:30 +0100

    sleep 1 minut

--- youtube_views.py
+++ youtube_views.py
@@ -7,4 +7,5 @@ driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver'
 driver.get('https://www.youtube.com/watch?v=mlZj910ey-I')
 watch_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-cued-thumbnail-overlay > button')
 watch_vedio.click()
-for i in range(100)
+for i in range(100):
+    time.sleep(60)
