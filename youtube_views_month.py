1ca96409630dc6be816ddedf87c410139b281594
aminedachir <dachiramine110@gmail.com>
2021-07-05 13:58:12 +0100

    print len of all videos in chaine

--- youtube_views_month.py
+++ youtube_views_month.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(executable_path =  '/home/amine/Downloads/Chromedriver')
+driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
 driver.get('https://www.youtube.com/channel/UCaj2GeIsW6Xll26js9o4seQ')
+videos = driver.find_element(By.CSS_SELECTOR, '#tabsContent > tp-yt-paper-tab:nth-child(4) > div')
+videos.click()
+all_videos = driver.find_element(By.CSS_SELECTOR, '#meta')
+print(len(all_videos))
