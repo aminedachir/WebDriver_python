6f36bfe2312b53ef0377de940aaaa87735b74df1
aminedachir <dachiramine110@gmail.com>
2021-07-06 14:50:47 +0100

    practice

--- practice.py
+++ practice.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
-
 driver.get('http://127.0.0.1:5000/')
 time.sleep(1)
 emails = ['amine@gmail.com','mohamed@gmail.com','yassin@gmail.com','ahmed@gmail.com','hichem@gmail.com']
@@ -17,7 +16,6 @@ for i in range(5):
     password_field.send_keys("amineamine")
     time.sleep(0.5)
     email_field.send_keys(Keys.ENTER)
-
     time.sleep(0.5)
 time.sleep(1)
 driver.quit()
