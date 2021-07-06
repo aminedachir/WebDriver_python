fb1870fc8f1d8d36ed3857799d94dda84a65efd5
aminedachir <dachiramine110@gmail.com>
2021-07-06 14:47:54 +0100

    sleep 3 secondes and refresh page

--- youtube_views.py
+++ youtube_views.py
@@ -6,9 +6,10 @@ import time
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
 youtube_vedio_url = input("enter your url vedio :")
+
 driver.get(youtube_vedio_url)
 watch_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-cued-thumbnail-overlay > button')
 watch_vedio.click()
 for i in range(100):
-    time.sleep(2)
+    time.sleep(3)
     driver.refresh()
