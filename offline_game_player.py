0eb08177e764da73d0d205e4eefc1a61b77016c2
aminedachir <dachiramine110@gmail.com>
2021-07-06 14:49:38 +0100

    problem

--- offline_game_player.py
+++ offline_game_player.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
