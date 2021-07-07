9b3b6cca881aeb710fdd3602dce2a03f33179d2b
aminedachir <dachiramine110@gmail.com>
2021-07-07 12:32:48 +0100

    Update

--- practice.py
+++ practice.py
@@ -6,14 +6,14 @@ import time
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 driver.get('http://127.0.0.1:5000/')
 time.sleep(1)
-emails = ['amine@gmail.com','mohamed@gmail.com','yassin@gmail.com','ahmed@gmail.com','hichem@gmail.com']
+emails = ['admin','mohamed@gmail.com','yassin@gmail.com','ahmed@gmail.com','hichem@gmail.com']
 for i in range(5):
     email = emails[i]
     email_field = driver.find_element(By.ID, 'email')
     email_field.send_keys(email)
     time.sleep(0.5)
     password_field = driver.find_element(By.ID, 'password')
-    password_field.send_keys("amineamine")
+    password_field.send_keys("admin")
     time.sleep(0.5)
     email_field.send_keys(Keys.ENTER)
     time.sleep(0.5)
--- youtube_views.py
+++ youtube_views.py
@@ -10,6 +10,7 @@ youtube_vedio_url = input("enter your url vedio :")
 driver.get(youtube_vedio_url)
 watch_vedio = driver.find_element(By.CSS_SELECTOR, '#movie_player > div.ytp-cued-thumbnail-overlay > button')
 watch_vedio.click()
+
 for i in range(100):
     time.sleep(3)
     driver.refresh()
