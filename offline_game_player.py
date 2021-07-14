9bc9739c684895f681e6e934d29bf1590b4802b7
aminedachir <dachiramine110@gmail.com>
2021-07-14 11:51:46 +0100

    play

--- offline_game_player.py
+++ offline_game_player.py
@@ -1,9 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
-
 driver.get('http://fb.com')
 time.sleep(3)
 for i in range (10):
