8b531a05a3802758b4f0c7fa41e7c788bc8c1122
aminedachir <dachiramine110@gmail.com>
2021-07-15 15:40:42 +0100

    problem

--- paypal_users.py
+++ paypal_users.py
@@ -17,6 +17,6 @@ phone_number_input.send_keys(phone_number)
 
 submit = driver.find_element(By.CSS_SELECTOR, '#content > div:nth-child(1) > div > form > div.action > input')
 submit.click()
- time.sleep(4)
+time.sleep(4)
 text = driver.find_element(By.CSS_SELECTOR, '#challenges > div > h1')
 print(text.text)
