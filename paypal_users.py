a0cc69424a71fe4f0316135173d1da19e475e6e9
aminedachir <dachiramine110@gmail.com>
2021-07-15 14:23:14 +0100

    submit

--- paypal_users.py
+++ paypal_users.py
@@ -11,3 +11,4 @@ phone_number_input = driver.find_element(By.CSS_SELECTOR, '#email')
 phone_number_input.send_keys(phone_number)
 
 submit = driver.find_element(By.CSS_SELECTOR, '#btnNext')
+submit.click()
