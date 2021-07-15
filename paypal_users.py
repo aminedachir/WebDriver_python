b0ba622a73ef762d695b26692e759da8b235ce34
aminedachir <dachiramine110@gmail.com>
2021-07-15 14:13:31 +0100

    input

--- paypal_users.py
+++ paypal_users.py
@@ -3,3 +3,5 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
 driver.get('https://www.paypal.com/dz/signin')
+
+phone_number = input("Enter a phone number : ")
