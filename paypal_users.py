941886aceddd3ed9a23f8dce1a05e6b7942f7e0b
aminedachir <dachiramine110@gmail.com>
2021-07-15 15:53:06 +0100

    finish

--- paypal_users.py
+++ paypal_users.py
@@ -8,22 +8,18 @@ from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
-driver.get('https://www.paypal.com/authflow/password-recovery/?country.x=DZ&locale.x=en_US&redirectUri=%252Fsignin%253FZ3JncnB0%253D%2526cHJwPXJwdA%253D')
-
-phone_number = input("Enter a phone number : ")
-
-phone_number_input = driver.find_element(By.CSS_SELECTOR, '#email')
-phone_number_input.send_keys(phone_number)
-
-submit = driver.find_element(By.CSS_SELECTOR, '#content > div:nth-child(1) > div > form > div.action > input')
-submit.click()
-time.sleep(4)
-
-try:
-    text = driver.find_element(By.CSS_SELECTOR, '#challenges > div > h1')
-    print("True")
-except NoSuchElementException:
-    print("false")
-
-time.sleep(3)
+for i in range(100):
+    driver.get('https://www.paypal.com/authflow/password-recovery/?country.x=DZ&locale.x=en_US&redirectUri=%252Fsignin%253FZ3JncnB0%253D%2526cHJwPXJwdA%253D')
+    phone_number = input("Enter a phone number : ")
+    phone_number_input = driver.find_element(By.CSS_SELECTOR, '#email')
+    phone_number_input.send_keys(phone_number)
+    submit = driver.find_element(By.CSS_SELECTOR, '#content > div:nth-child(1) > div > form > div.action > input')
+    submit.click()
+    time.sleep(4)
+    try:
+        text = driver.find_element(By.CSS_SELECTOR, '#challenges > div > h1')
+        print("True")
+    except NoSuchElementException:
+        print("false")
+    time.sleep(1)
 
