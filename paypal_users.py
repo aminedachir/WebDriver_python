d8d85c8877022cff22df0e9a3b381c962ee06bbd
aminedachir <dachiramine110@gmail.com>
2021-07-15 15:12:39 +0100

    phone confirmation

--- paypal_users.py
+++ paypal_users.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
@@ -12,3 +14,9 @@ phone_number_input.send_keys(phone_number)
 
 submit = driver.find_element(By.CSS_SELECTOR, '#content > div:nth-child(1) > div > form > div.action > input')
 submit.click()
+
+try:
+    phone = driver.find_element(By.CSS_SELECTOR, '#challenges > div > div.action > button')
+    phone.click()
+except NoSuchElementException:
+    print("FLASE")
