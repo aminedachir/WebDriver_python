dbc9bad97ed8568d634addd13e8316da9f31bac5
aminedachir <dachiramine110@gmail.com>
2021-07-22 15:48:43 +0100

    enter name of product and checkin aliexpress

--- compare_products.py
+++ compare_products.py
@@ -1,1 +1,7 @@
 from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')$
+
+product = input("complete name of product :")
+
+driver.get("https://www.aliexpress.com/")
