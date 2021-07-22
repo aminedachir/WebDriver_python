5bc4b641fd11e3702c7b9a4d000fd07fb461d5ec
aminedachir <dachiramine110@gmail.com>
2021-07-22 15:55:43 +0100

    search

--- compare_products.py
+++ compare_products.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
 product = input("complete name of product :")
@@ -8,5 +9,6 @@ product = input("complete name of product :")
 driver.get("https://www.aliexpress.com/")
 
 search_for_product = driver.find_element(By.CLASS_NAME, 'search-key')
+search_for_product.send_keys(product)
 
 
