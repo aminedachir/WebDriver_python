68ab27ac0ee4879321fe2b5ae3c41ae8a13f911b
aminedachir <dachiramine110@gmail.com>
2021-07-23 14:09:36 +0100

    remove ads

--- compare_products.py
+++ compare_products.py
@@ -7,14 +7,14 @@ driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver'
 
 product = input("complete name of product :")
 
-driver.get("https://www.aliexpress.com/")
+driver.get("https://www.Overstock.com/")
 time.sleep(2)
 
-pub1 = driver.find_element(By.CSS_SELECTOR, 'body > div:nth-child(14) > div > div._1-aaU > div._1ZwH_ > div.undefined._1-SOk')
+pub1 = driver.find_element(By.CSS_SELECTOR, '#notification_allow')
 pub1.click()
 
-pub = driver.find_element(By.CSS_SELECTOR, 'body > div:nth-child(16) > div > div > img.btn-close')
-pub.click()
+#pub = driver.find_element(By.CSS_SELECTOR, 'body > div:nth-child(16) > div > div > img.btn-close')
+#pub.click()
 
 search_for_product = driver.find_element(By.CLASS_NAME, 'search-key')
 search_for_product.send_keys(product)
