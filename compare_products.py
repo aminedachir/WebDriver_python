520107d4ed8e29e3f0cadd9f03f56be469115c65
aminedachir <dachiramine110@gmail.com>
2021-07-25 16:34:32 +0100

    find year with XPATH

--- compare_products.py
+++ compare_products.py
@@ -15,8 +15,9 @@ search_for_product.send_keys(Keys.ENTER)
 
 price = driver.find_element(By.CSS_SELECTOR, '#results-list > div:nth-child(1) > div > div.listing-item > div.block.item-title > div > span')
 print(price.text)
-color = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/ul[1]/li[1]/strong')
+year = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/ul[1]/li[1]/strong')
+print(year.text)
+color = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/ul[2]/li[2]/strong')
 print(color.text)
-#year =
 #kilometers =
 #doors =
