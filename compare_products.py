99875f857102fe3d42b1ddda848d77fda4609a82
aminedachir <dachiramine110@gmail.com>
2021-07-25 16:29:40 +0100

    scrap price of car

--- compare_products.py
+++ compare_products.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
 product = input("complete name of product :")
@@ -14,3 +13,5 @@ search_for_product = driver.find_element(By.CLASS_NAME, 'Searchbox__keyword__inp
 search_for_product.send_keys(product)
 search_for_product.send_keys(Keys.ENTER)
 
+price = driver.find_element(By.CSS_SELECTOR, '#results-list > div:nth-child(1) > div > div.listing-item > div.block.item-title > div > span')
+print(price.text)
