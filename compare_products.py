05c618098595e9cabafe8effbc4b6b617ab5be34
aminedachir <dachiramine110@gmail.com>
2021-07-25 17:01:05 +0100

    search by name of car and model of car

--- compare_products.py
+++ compare_products.py
@@ -5,12 +5,13 @@ import time
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
 
-product = input("complete name of product :")
+car = input("name of car :")
+model = input("Model of car :")
 
 driver.get("https://dubai.dubizzle.com/")
 
 search_for_product = driver.find_element(By.CLASS_NAME, 'Searchbox__keyword__input')
-search_for_product.send_keys(product)
+search_for_product.send_keys(car," ", model)
 search_for_product.send_keys(Keys.ENTER)
 
 price = driver.find_element(By.CSS_SELECTOR, '#results-list > div:nth-child(1) > div > div.listing-item > div.block.item-title > div > span')
@@ -20,3 +21,5 @@ kilometers = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/d
 doors = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/ul[2]/li[1]')
 print("Price :",price.text,",", year.text,",", color.text,",", kilometers.text,",", doors.text)
 time.sleep(1)
+
+driver.get('https://uae.yallamotor.com/')
