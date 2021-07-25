746401d01150cacfd86a07b49e4b7592dd9571ba
aminedachir <dachiramine110@gmail.com>
2021-07-25 17:03:08 +0100

    import NoSuchElementException

--- compare_products.py
+++ compare_products.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 driver = webdriver.Chrome(executable_path = '/home/amine/Downloads/chromedriver')
