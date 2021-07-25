60a1ab7ce1a397c9c0059b9a20bdbdae55293a07
aminedachir <dachiramine110@gmail.com>
2021-07-25 17:06:02 +0100

    if NoSuchElementException print noy found

--- compare_products.py
+++ compare_products.py
@@ -15,12 +15,15 @@ search_for_product = driver.find_element(By.CLASS_NAME, 'Searchbox__keyword__inp
 search_for_product.send_keys(car," ", model)
 search_for_product.send_keys(Keys.ENTER)
 
-price = driver.find_element(By.CSS_SELECTOR, '#results-list > div:nth-child(1) > div > div.listing-item > div.block.item-title > div > span')
-year = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/ul[1]/li[1]')
-color = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/ul[2]/li[2]')
-kilometers = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/ul[1]/li[2]')
-doors = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/ul[2]/li[1]')
-print("Price :",price.text,",", year.text,",", color.text,",", kilometers.text,",", doors.text)
-time.sleep(1)
+try:
+    price = driver.find_element(By.CSS_SELECTOR, '#results-list > div:nth-child(1) > div > div.listing-item > div.block.item-title > div > span')
+    year = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/ul[1]/li[1]')
+    color = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/ul[2]/li[2]')
+    kilometers = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/ul[1]/li[2]')
+    doors = driver.find_element(By.XPATH, '//*[@id="results-list"]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/ul[2]/li[1]')
+    print("Price :",price.text,",", year.text,",", color.text,",", kilometers.text,",", doors.text)
+    time.sleep(1)
+except NoSuchElementException:
+    print("No resulte fond")
 
 driver.get('https://uae.yallamotor.com/')
